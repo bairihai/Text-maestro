b0661aa397ae621fc765219092704c4acc298c4e
bairihai <237876867@qq.com>
2024-05-12 11:57:46 +0800

    再给react-dom升个级 完蛋哩 没大用

--- electron-app-v2/app/renderer/app.jsx
+++ electron-app-v2/app/renderer/app.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/">成功了吗？?</Route>
+        <Route path="/" element={<div>成功了吗？</div>} />
       </Routes>
     </Router>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
