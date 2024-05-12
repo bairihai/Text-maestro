e2b5c0c536bcc4d48191fb78f57ce14018a4c9b5
bairihai <237876867@qq.com>
2024-05-12 15:54:33 +0800

    再也不折腾了 老版本老老实实写router

---
+++ electron-app/app/renderer/router.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Root from './container/root';
+// import Resume from './container/resume';
+
+function Router() {
+  return (
+    <HashRouter>
+      <Switch>
+        {/* 一定要添加 exact */}
+        <Route path="/" exact>
+          <Root />
+        </Route>
+        <Route path="/resume" exact>
+          <Resume />
+        </Route>
+      </Switch>
+      {/* 默认重定向到首页 */}
+      <Redirect to="/" />
+    </HashRouter>
+  );
+}
+export default Router;
