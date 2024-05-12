148fa4b7104cf572d09acf79d9cbeff367ab2ec5
bairihai <237876867@qq.com>
2024-05-12 14:01:08 +0800

    升级router给render干报废了 我看v2多半也得寄了

--- electron-app-v2/app/renderer/app.jsx
+++ electron-app-v2/app/renderer/app.jsx
@@ -6,11 +6,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>成功了吗？</div>} />
+        <Route path="/" element={<div>成功了吗？</div>} exact />
       </Routes>
     </Router>
   );
 }
 
+export default App;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
