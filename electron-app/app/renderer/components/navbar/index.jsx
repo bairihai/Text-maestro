b9d7e439b05d0e191d2f74881034dd3009d68845
bairihai <237876867@qq.com>
2024-05-15 10:13:32 +0800

    【不可用】web测试，新增部分router页面。目前已知push功能不可用，路由功能不可用。

--- electron-app/app/renderer/components/navbar/index.jsx
+++ electron-app/app/renderer/components/navbar/index.jsx
@@ -3,74 +3,74 @@ import React from 'react';
 import './index.less';
 
 import { Tabs, Typography } from "@arco-design/web-react";
-import {
-    IconDriveFile,
-    IconClockCircle,
-    IconFolder,
-} from "@arco-design/web-react/icon";
+
 const TabPane = Tabs.TabPane;
-const style = {
-    // textAlign: "center",
-    // marginTop: 20,
-};
 
 const { Text } = Typography;
 
 
 import { Menu } from "@arco-design/web-react";
 import {
-    IconApps,
+    IconDriveFile,
+    IconFolder,
+
     IconSafe,
-    IconBulb,
-    IconRobot,
+
+    IconSettings,
     IconFire,
 } from "@arco-design/web-react/icon";
 const MenuItem = Menu.Item;
 const SubMenu = Menu.SubMenu;
 
 
+import { useHistory } from 'react-router';
+
+
 const NavBar = () => {
+    const history = useHistory(); // 使用 useHistory 钩子 版本问题懒得改了（这都是屎山改不动的）
 
     return (
         // <div className="menu-demo-round" style={{ height: '100%' , width: 200 }}>
-            <Menu style={{ height: '100%', width: 200 }} mode='vertical' hasCollapseButton>
+            <Menu style={{ height: '100%', width: 225 }} mode='vertical' hasCollapseButton>
                 <MenuItem key="0">
-                    <IconApps />
-                    Navigation 1
+                    <IconFolder />
+                    单一文件夹分析
                 </MenuItem>
                 <SubMenu
                     key="1"
                     title={
                         <>
-                            <IconRobot />
-                            Navigation 2
+                            <IconDriveFile />
+                            复合web分析
                         </>
                     }
                 >
-                    <MenuItem key="1_0">Beijing</MenuItem>
-                    <MenuItem key="1_1">Shanghai</MenuItem>
-                    <MenuItem key="1_2">Guangzhou</MenuItem>
+                    <MenuItem key="1_0">discord服务器聊天记录</MenuItem>
+                    <MenuItem key="1_1">知乎用户回答</MenuItem>
+                    <MenuItem key="1_2">b站收藏夹</MenuItem>
                 </SubMenu>
                 <SubMenu
                     key="2"
                     title={
                         <>
-                            <IconBulb />
-                            Navigation 3
+                            <IconDriveFile />
+                            复合本地文档分析
                         </>
                     }
                 >
-                    <MenuItem key="2_0">Wuhan</MenuItem>
-                    <MenuItem key="2_1">Chengdu</MenuItem>
+                    <MenuItem key="2_0">obsidian仓库</MenuItem>
+                    <MenuItem key="2_1">（待更新）</MenuItem>
                 </SubMenu>
-                <MenuItem key="3">
-                    <IconSafe />
-                    Navigation 4
-                </MenuItem>
-                <MenuItem key="4">
-                    <IconFire />
-                    Navigation 5
-                </MenuItem>
+
+                <MenuItem key="3" onClick={() => history.push('/document')}>
+                <IconSafe />
+                官网丨文档丨帮助
+            </MenuItem>
+            
+            <MenuItem key="4" onClick={() => history.push('/')}>
+                <IconSettings />
+                Text-maestro设置
+            </MenuItem>
             </Menu>
         // </div>
     );
