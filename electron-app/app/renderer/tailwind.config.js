cad187443c54c236741966045eb8f5a3b44933d1
bairihai <237876867@qq.com>
2024-06-09 05:42:34 +0800

    tailwindcss config

---
+++ electron-app/app/renderer/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: 'jit',
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        backgroundImage: {
+          stripe:
+            'repeating-linear-gradient(-45deg, #f3f3f3, #f3f3f3 0.5rem, transparent 0.5rem, transparent 1rem)',
+        },
+        transitionDuration: {
+          DEFAULT: '150ms',
+        },
+      },
+    },
+    darkMode: 'class',
+    plugins: [],
+}
