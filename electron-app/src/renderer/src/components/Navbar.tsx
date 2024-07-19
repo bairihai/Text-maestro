eee713120afacf2558d03ac381bb21c734bf02e1
bairihai <237876867@qq.com>
2024-07-19 20:37:52 +0800

    【可用】【重要】底层优化、navbar最终版

--- electron-app/src/renderer/src/components/Navbar.tsx
+++ electron-app/src/renderer/src/components/Navbar.tsx
@@ -27,76 +27,75 @@ export const LINKS: { // 自用数组，用于生成导航里的链接。
     title: string // 或者命名为label。
     icon: JSX.Element // 写一个元素，形如</div>。另一种写法是IconName组件类型，但是兼容性不好，所以不了。
     items?: { key: string; label: string }[]; // 可选项，子菜单。
-  }[] = [
-    {
-        key: '0',
-        title: '游戏文件',
-        icon: <IconDriveFile />,
-        items: [
-            { key: '0_0', label: 'maa 明日方舟库存管理' },
-            { key: '0_1', label: '明日方舟 寻访记录管理' }
-        ]
-    },
-    {
-        key: '1',
-        title: '论坛app',
-        icon: <IconDriveFile />,
-        items: [
-            { key: '1_0', label: 'b站评论' },
-            { key: '1_1', label: '知乎用户回答' },
-            { key: '1_2', label: 'b站收藏夹' }
-        ]
-    },
-    {
-        key: '2',
-        title: '聊天app',
-        icon: <IconDriveFile />,
-        items: [
-            { key: '2_0', label: 'discord聊天记录' },
-            { key: '2_1', label: 'qq聊天记录' },
-            { key: '2_2', label: '微信聊天记录' }
-        ]
-    },
-    {
-        key: '3',
-        title: '通用文档',
-        icon: <IconDriveFile />,
-        items: [
-            { key: '3_0', label: '任一文件夹的结构树' },
-            { key: '3_1', label: 'obsidian 单篇文档分析' },
-            { key: '3_2', label: 'obsidian 多文档分析' },
-            { key: '3_3', label: '掘金小册 上云action生成' },
-            { key: '3_4', label: 'Hiplot 作图meta生成' },
-            { key: '3_5', label: 'Apifox postman双向配置改造' }
-        ]
-    },
-    {
-        key: '5',
-        to: '/document',
-        title: '官网丨文档丨帮助',
-        icon: <IconSafe />
-    },
-    {
-        key: '6',
-        to: '/1',
-        title: 'playground DIY广场',
-        icon: <IconSettings />
-    },
-    {
-        key: '7',
-        to: '/1',
-        title: 'Text-maestro设置',
-        icon: <IconSettings />
-    }
+}[] = [
+        {
+            key: '0',
+            title: '游戏文件',
+            icon: <IconDriveFile />,
+            items: [
+                { key: '0_0', label: 'maa 明日方舟库存管理' },
+                { key: '0_1', label: '明日方舟 寻访记录管理' }
+            ]
+        },
+        {
+            key: '1',
+            title: '论坛app',
+            icon: <IconDriveFile />,
+            items: [
+                { key: '1_0', label: 'b站评论' },
+                { key: '1_1', label: '知乎用户回答' },
+                { key: '1_2', label: 'b站收藏夹' }
+            ]
+        },
+        {
+            key: '2',
+            title: '聊天app',
+            icon: <IconDriveFile />,
+            items: [
+                { key: '2_0', label: 'discord聊天记录' },
+                { key: '2_1', label: 'qq聊天记录' },
+                { key: '2_2', label: '微信聊天记录' }
+            ]
+        },
+        {
+            key: '3',
+            title: '通用文档',
+            icon: <IconDriveFile />,
+            items: [
+                { key: '3_0', label: '任一文件夹的结构树' },
+                { key: '3_1', label: 'obsidian 单篇文档分析' },
+                { key: '3_2', label: 'obsidian 多文档分析' },
+                { key: '3_3', label: '掘金小册 上云action生成' },
+                { key: '3_4', label: 'Hiplot 作图meta生成' },
+                { key: '3_5', label: 'Apifox postman双向配置改造' }
+            ]
+        },
+        {
+            key: '5',
+            to: '/document',
+            title: '官网丨文档丨帮助',
+            icon: <IconSafe />
+        },
+        {
+            key: '6',
+            to: '/1',
+            title: 'playground DIY广场',
+            icon: <IconSettings />
+        },
+        {
+            key: '7',
+            to: '/1',
+            title: 'Text-maestro设置',
+            icon: <IconSettings />
+        }
 ];
+
 
 export const NavBar = () => {
     const navigateTo = useNavigate(); // 路由方法。navigateTo后面不要加括号，那是立即执行等号后者的写法。
 
-
-
     return (
-        // <div className="menu-demo-round" style={{ height: '100%' , width: 200 }}>
+        <div className="menu-demo-round" style={{ height: '100%', width: '245px', position: 'fixed', top: 0, left: 0 }}>
         <Menu style={{ height: '100%', width: 245 }} mode='vertical' hasCollapseButton>
             {LINKS.map((link) => {
                 if (link.items) {
@@ -117,7 +116,7 @@ export const NavBar = () => {
                     );
                 } else {
                     return (
-                        <MenuItem key={link.key} onClick={() => navigateTo(link.to)}>
+                        <MenuItem key={link.key} onClick={() => link.to && navigateTo(link.to)}>
                             {link.icon}
                             {link.title}
                         </MenuItem>
@@ -125,5 +124,6 @@ export const NavBar = () => {
                 }
             })}
         </Menu>
+        </div>
     );
 }
