d0c49cc4b62d8280ec30e5627f011c9018e714eb
bairihai <237876867@qq.com>
2024-08-16 18:38:56 +0800

    【可用】【重要】react redux 已经可以读取，接下来需要实现修改

--- electron-app/src/renderer/src/pages/playground/index.tsx
+++ electron-app/src/renderer/src/pages/playground/index.tsx
@@ -4,7 +4,11 @@ import { Input } from '@arco-design/web-react';
 // 不需要，我已经在app页面引入了arco css
 // import './index.less'; // 如果需要自定义样式，可以取消注释
 
+import { useSelector } from 'react-redux';
+
 function Playground() {
+  const appName = useSelector((state: any) => state.global.appName);
+  console.log('appName = ', appName);
   return (
     <div className="p-4"> {/* 使用tailwind的padding类 */}
       <Input placeholder="请输入内容" className="w-full" /> {/* 使用arco的Input组件 */}
